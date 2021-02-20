8574877af4f86b12e8854e97c5006ba022e0b38b
AndrewSimons3 <simdrew07@gmail.com>
2021-02-20 12:16:05 -0600

    comments for starting project

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-console.log('Hello World!');
-console.error("ooops something went wrong");
-console.dir({name: "Andrew"});
+// Problem: we need a simple way to look at a users badge count and javascript points
+// Solution: use node.js to connect to treehouse's API to get profile information to print out
+
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+// Read the data
+// Parse the data
+// Print the data
