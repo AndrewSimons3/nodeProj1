f546a63fe48f405127eeb79f74f459410fde9815
AndrewSimons3 <simdrew07@gmail.com>
2021-02-20 14:08:41 -0600

    printing out date for users

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Problem: we need a simple way to look at a users badge count and javascript points
 // Solution: use node.js to connect to treehouse's API to get profile information to print out
 const https = require('https');
-const username = "chalkers";
-
 
 function printMessage(username, badgeCount, points) {
    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in
@@ -10,8 +8,10 @@ function printMessage(username, badgeCount, points) {
    console.log(message);
 }
 
-// Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+
+function getProfile(username) {
+  // Connect to the API URL (https://teamtreehouse.com/username.json)
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
                           let body = "";
                           // Read the data
                           response.on('data', data => {
@@ -21,9 +21,14 @@ const request = https.get(`https://teamtreehouse.com/${username}.json`, response
                           response.on('end', () => {
                           // Parse the data
                           const profile = JSON.parse(body);
-                          console.dir(profile);
+                          
                           printMessage(username, profile.badges.length, profile.points.JavaScript);
                           // Print the data
                           });
                           
                           });
+                        };
+
+
+const users = process.argv.slice(2);
+users.forEach(getProfile);
