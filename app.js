434f7142628303f47524f168aa8f9b726433099a
AndrewSimons3 <simdrew07@gmail.com>
2021-02-24 22:04:09 -0600

    commit

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Problem: we need a simple way to look at a users badge count and javascript points
 // Solution: use node.js to connect to treehouse's API to get profile information to print out
 
-const profile = require('./profile.js');
+// const profile = require('./profile.js');
 
-const users = process.argv.slice(2);
-users.forEach(profile.get);
+// const users = process.argv.slice(2);
+// users.forEach(profile.get);
+
+
+//Problem
+//Solution
+
+
