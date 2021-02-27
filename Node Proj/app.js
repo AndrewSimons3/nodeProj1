ad914ba903e38f7440df428e45e65692a8960efc
AndrewSimons3 <simdrew07@gmail.com>
2021-02-27 09:52:49 -0600

    hash

--- Node Proj/app.js
+++ Node Proj/app.js
@@ -1,1 +1,8 @@
-var unsecurePlainTextPassword = "password"
+var unsecurePlainTextPassword = "password";
+
+var bcrypt = require('bcrypt');
+bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.hash(unsecurePlaintextPassword, salt, function(err, hash) {
+      console.log(hash)
+  });
+});
